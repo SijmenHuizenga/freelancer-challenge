362cb8a0a8ada53954ca988387966144752864c2
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-11-07 00:31:34 +0100

    Second version, fancy code, much quicker

--- sijmen.py
+++ sijmen.py
@@ -1,6 +1,5 @@
 import json
-
-transactions = []
+from typing import List
 
 ships = [
     {"price": 0, "capacity": 10},  # this is the default ship
@@ -9,8 +8,6 @@ ships = [
     {"price": 2000, "capacity": 30, "name": "HUMPBACK"},
 ]
 
-shipI = 0
-
 things = [
     "ore",
     "water",
@@ -18,147 +15,123 @@ things = [
     "contraband"
 ]
 
-def makeTransaction(planet, transaction):
-    x = {
-        "planet": planet["name"],
-        "deltaOre": transaction["ore"],
-        "deltaWater": transaction["water"],
-        "deltaEngineParts": transaction["engineparts"],
-        "deltaContraband": transaction["contraband"],
-    }
-    if "shipPurchase" in transaction:
-        x["shipPurchase"] = transaction["shipPurchase"]
-    print("Transaction", x)
-    transactions.append(x)
+def knaphoor(cost: int, profit: int, inventory: List[int], inventoryCapacity: int,
+             budget: int, prices: List[int], profits: List[int], continuationI: int = 0) -> (int, int, List[int],):
+
+    if cost > budget:
+        return 999999, 0, []
+
+    if inventoryCapacity == 0:
+        return cost, profit, inventory
+
+    bestProfit = profit
+    bestCost = cost
+    bestInventory = inventory
+    for i in range(continuationI, len(prices)):
+        newCost, newProfit, newInventory = knaphoor(cost + prices[i],
+                                                    profit + profits[i],
+                                                    [(inventory[j] + 1 if j == i else inventory[j])
+                                                     for j in range(len(inventory))],
+                                                    inventoryCapacity - 1,
+                                                    budget, prices, profits, i)
+        if newProfit > bestProfit:
+            bestProfit = newProfit
+            bestCost = newCost
+            bestInventory = newInventory
+    return bestCost, bestProfit, bestInventory
 
 
-with open('starmap.json') as json_file:
-    starmap = json.load(json_file)
-    i = 0
-    inventory = {thing: 1 for thing in things}
-    inventory["contraband"] = 0
+def solve():
+    transactions = []
+    shipI = 0
 
-    balance = 0
+    def makeTransaction(planet, transaction):
+        x = {
+            "planet": planet["name"],
+            "deltaOre": transaction["ore"],
+            "deltaWater": transaction["water"],
+            "deltaEngineParts": transaction["engineparts"],
+            "deltaContraband": transaction["contraband"],
+        }
+        if "shipPurchase" in transaction:
+            x["shipPurchase"] = transaction["shipPurchase"]
+        transactions.append(x)
 
-    for currentStar in starmap:
-        # sell everything
-        t = {thing: -1 * inventory[thing] for thing in things}
-        for thing in things:
-            balance += inventory[thing] * currentStar[thing + "Price"]
-        inventory = {thing: 0 for thing in things}
+    with open('starmap.json') as json_file:
+        starmap = json.load(json_file)
+        starI = 0
+        inventory = {thing: 1 for thing in things}
+        inventory["contraband"] = 0
 
-        # check if on last star
-        if i == len(starmap) - 1:
+        balance = 0
+
+        for currentStar in starmap:
+            # sell everything
+            t = {thing: -1 * inventory[thing] for thing in things}
+            for thing in things:
+                balance += inventory[thing] * currentStar[thing + "Price"]
+            inventory = {thing: 0 for thing in things}
+
+            # check if on last star
+            if starI == len(starmap) - 1:
+                makeTransaction(currentStar, t)
+                continue
+                pass
+
+            nextStar = starmap[starI + 1]
+
+            # if we still have money left to buy a ship, lets do it!
+            if shipI < len(ships) - 1 and balance >= ships[shipI + 1]["price"] + 50:
+                balance -= ships[shipI + 1]["price"]
+                shipI += 1
+                t["shipPurchase"] = ships[shipI]["name"]
+
+            capacity = ships[shipI]["capacity"]
+
+            # do not buy things that won't make profit
+            # do not buy contraband
+            buyableThings = [thing for thing in things
+                             if currentStar[thing + "Price"] != 0
+                             and (nextStar[thing + "Price"] - currentStar[thing + "Price"]) > 0]
+            buyableThings.sort(key=lambda thing: nextStar[thing + "Price"] - currentStar[thing + "Price"])
+
+            prices = [currentStar[thing + "Price"] for thing in buyableThings]
+            profits = [nextStar[thing + "Price"] - currentStar[thing + "Price"] for thing in buyableThings]
+
+            # Find the best thing we can buy
+            cost, profit, shoppingList = knaphoor(
+                cost=0,
+                profit=0,
+                inventory=[0] * len(prices),
+                inventoryCapacity=capacity,
+                budget=balance,
+                prices=prices,
+                profits=profits)
+
+            # buy it
+            for i in range(len(buyableThings)):
+                # add to transaction
+                t[buyableThings[i]] += shoppingList[i]
+
+                # remove from inventory
+                inventory[buyableThings[i]] += shoppingList[i]
+
+            # update balance
+            balance -= cost
+
             makeTransaction(currentStar, t)
-            continue
-            pass
+            starI += 1
 
-        nextStar = starmap[i + 1]
+    output = {
+        "name": "Delftsche Zwervers (Sijmen Huizenga)",
+        "email": "sijmenhuizenga@gmail.com",
+        "transactions": transactions
+    }
 
-        # if we still have money left to buy a ship, lets do it!
-        if shipI < len(ships) - 1 and balance >= ships[shipI + 1]["price"] + 50:
-            balance -= ships[shipI + 1]["price"]
-            shipI += 1
-            t["shipPurchase"] = ships[shipI]["name"]
+    print("Total balance: " + str(balance))
 
-        capacity = ships[shipI]["capacity"]
-
-        # Calculate how much we will gain per piece of each thing
-        winstMargins = {thing: nextStar[thing + "Price"] - currentStar[thing + "Price"]
-                        for thing in things if currentStar[thing + "Price"] != 0}
-        winstMarginsKeys = list(winstMargins.keys())
-        winstMarginsVals = list(winstMargins.values())
-
-        # best shoppinglist starts out with all 0
-        bestShoppinglist = {thing: 0 for thing in things if currentStar[thing + "Price"] != 0}
-        bestWinst = 0
-
-        def magic(margin_counter, commulative_shoppinglist):
-
-            if margin_counter == len(winstMargins):
-                # shoppinglist complete!
-                global balance
-                global bestWinst
-                global bestShoppinglist
-
-                nr_of_items_bought = sum(commulative_shoppinglist.values())
-                if nr_of_items_bought > capacity:
-                    # too many things in our shopping cart
-                    return
-
-                cost = 0
-                for key in winstMarginsKeys:
-                    cost += currentStar[key + "Price"] * commulative_shoppinglist[key]
-                if cost > balance:
-                    # this setup is too expensive
-                    return
-
-                winst = 0
-                for key in winstMarginsKeys:
-                    winst += winstMargins[key] * commulative_shoppinglist[key]
-                if winst < bestWinst:
-                    return
-
-                # this is the best shoppinglist (until now)
-                bestShoppinglist = commulative_shoppinglist
-                bestWinst = winst
-
-                return
-            for k in range(capacity+1):
-                magic(margin_counter+1, {**commulative_shoppinglist, winstMarginsKeys[margin_counter]: k})
+    with open('output.json', 'w') as outfile:
+        json.dump(output, outfile)
 
 
-        magic(0, {thing: 0 for thing in things if currentStar[thing + "Price"] != 0})
-
-        for thing in bestShoppinglist:
-            t[thing] += bestShoppinglist[thing]
-            inventory[thing] += bestShoppinglist[thing]
-            balance -= bestShoppinglist[thing] * currentStar[thing+"Price"]
-
-        # print("Going to buy:")
-        # print(bestShoppinglist)
-
-        # bestInvestment = max(winstMargins.items(), key=operator.itemgetter(1))[0]
-        #
-        # # See how much we can buy of that thing
-        # howMuchBuy = int(min(balance / currentStar[bestInvestment + "Price"], ships[shipI]["capacity"]))
-        # print(currentStar["name"] + "\t investing in " + bestInvestment + " bying " + str(howMuchBuy))
-        #
-        # # buy that thing
-        # t[bestInvestment] = t[bestInvestment] + howMuchBuy
-        # balance -= howMuchBuy * currentStar[bestInvestment + "Price"]
-        # inventory[bestInvestment] += howMuchBuy
-
-        makeTransaction(currentStar, t)
-        i += 1
-
-output = {
-    "name": "Delftsche Zwervers (Sijmen Huizenga)",
-    "email": "sijmenhuizenga@gmail.com",
-    "transactions": transactions
-}
-
-print("Total balance: " + str(balance))
-
-with open('output.json', 'w') as outfile:
-    json.dump(output, outfile)
-
-# bewaren tot de eerste piek
-# koopt geen spullen op
-# je koopt iets niet als het in de volgende goedkoper te kopen zijn
-
-# REGELS
-# koop niet als de volgende planeet als de volgende planeet het voor een lagere heeft.
-#   goedkoper op andere planeet
-#   alleen bij een hogere prijs
-
-# verkopen op een piek
-#   de prijs bij de volgende planeet gelijk dan verkopen. ivm tijdelijke bagageprijs
-
-# Als de prijs daalt moet je altijd je spullen verkopen
-# je moet altijd verkopen als de prijs daalt
-# Nooit een goed kopen wat je dan verkoopt
-# Als de volgende planeet gelijke prijs of lager heeft dan
-
-
-# als je in een dal zit, dan moet je altijd kopen.
+solve()
